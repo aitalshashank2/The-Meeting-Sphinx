c2c7357ab77a53b8e4cf708844ab373a8282e527
Ayushman <ayushmantripathy9@gmail.com>
2021-05-09 18:40:54 +0530

    :tada: Implemented Recording Actions 1. Start Action 2. Stop Action 3. IsInSafeMethods permission

--- code/TheSphinx/permissions.py
+++ code/TheSphinx/permissions.py
@@ -7,6 +7,12 @@ class HasMeetingAccess(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.user in obj.organizers.all()) or (request.user in obj.attendees.all())
 
+
 class HasMessageAccess(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user is obj.sender
+
+
+class IsInSafeMethods(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS
--- code/TheSphinx/views/recording.py
+++ code/TheSphinx/views/recording.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from TheSphinx.models import Recording
+from TheSphinx.models import Recording, Meeting
 from TheSphinx.serializers import RecordingGetSerializer, RecordingPostSerializer
+from TheSphinx.permissions import IsInSafeMethods
 
 
 class RecordingViewSet(viewsets.ModelViewSet):
@@ -14,9 +19,41 @@ class RecordingViewSet(viewsets.ModelViewSet):
             return RecordingGetSerializer
 
     queryset = Recording.objects.all()
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsInSafeMethods, ]
 
     def perform_create(self, serializer):
         serializer.save(
             user=self.request.user
         )
+    
+    @action(detail=False, methods=['post'])
+    def start(self, request):
+        try:
+            code = request.data['meeting_code']
+        except KeyError:
+            return Response("Meeting Code Not Provided!", status=400)
+        try:
+            m = Meeting.objects.get(meeting_code=code)
+        except Meeting.DoesNotExist:
+            return Response("No such meeting exists!", status=404)
+            
+        r = Recording(
+            user=request.user,
+            meeting=m,
+        )
+        r.save()
+
+        return Response({
+            'data': f'User {request.user.username} banned from {code}'
+        }, status=202)
+
+    @action(detail=True, methods=['get'])
+    def stop(self, request, pk):
+        try:
+            r = Recording.objects.get(pk=pk)
+        except Recording.DoesNotExist:
+            return Response("No such recording instance exists!",status=404)
+        
+        r.end_time = datetime.now()
+        r.save()
+        return Response(f"Recording {r.id} stopped by user!",status=200)
