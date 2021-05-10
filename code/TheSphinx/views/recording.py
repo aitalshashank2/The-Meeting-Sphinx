c02b7c7ac3b1cbac48fdc840ffe43b83bbc70608
Shashank Aital <aitalshashank2@gmail.com>
2021-05-10 16:40:18 +0530

    Recording detection (#10)
    * :tada: Implemented Recording Actions
    1. Start Action
    2. Stop Action
    3. IsInSafeMethods permission
    
    * :rocket: Made API endpoints for recording detection
    
    * :rocket: Made API endpoints for recording detection
    
    * :white_check_mark: Websocket implemented
    
    * :bookmark: Refactor
    
    Co-authored-by: Ayushman <ayushmantripathy9@gmail.com>

--- code/TheSphinx/views/recording.py
+++ code/TheSphinx/views/recording.py
@@ -25,35 +25,3 @@ class RecordingViewSet(viewsets.ModelViewSet):
         serializer.save(
             user=self.request.user
         )
-    
-    @action(detail=False, methods=['post'])
-    def start(self, request):
-        try:
-            code = request.data['meeting_code']
-        except KeyError:
-            return Response("Meeting Code Not Provided!", status=400)
-        try:
-            m = Meeting.objects.get(meeting_code=code)
-        except Meeting.DoesNotExist:
-            return Response("No such meeting exists!", status=404)
-            
-        r = Recording(
-            user=request.user,
-            meeting=m,
-        )
-        r.save()
-
-        return Response({
-            'data': f'User {request.user.username} banned from {code}'
-        }, status=202)
-
-    @action(detail=True, methods=['get'])
-    def stop(self, request, pk):
-        try:
-            r = Recording.objects.get(pk=pk)
-        except Recording.DoesNotExist:
-            return Response("No such recording instance exists!",status=404)
-        
-        r.end_time = datetime.now()
-        r.save()
-        return Response(f"Recording {r.id} stopped by user!",status=200)
